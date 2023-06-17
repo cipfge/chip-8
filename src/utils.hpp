a7fd36591bf58225e364e2daf7dd41cf9643f438
Ciprian Fuge <ciprianfuge@gmail.com>
2023-06-17 16:56:20 +0300

    Remove static keyword from utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -6,10 +6,10 @@
 namespace utils
 {
 
-static inline uint32_t get_file_size(const std::string& file_path)
+inline uint32_t get_file_size(const std::string& file_path)
 {
     std::filesystem::path path { file_path };
     return std::filesystem::file_size(path);
 }
 
-}
+} // namespace utils
